109a0d315e79f7344dac18b2981921531b75b86e
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-08-09 19:11:46 +0430

    income and expense updated

--- src/components/TransActionComponent/TransActionComponent.js
+++ src/components/TransActionComponent/TransActionComponent.js
@@ -1,5 +1,11 @@
-const TransActionComponent = ({transactions}) => {
-  return <section>transactions!</section>;
+const TransActionComponent = ({ transactions }) => {
+  return (
+    <div>
+      {transactions.map((t) => {
+        return <div key={t.id}>{t.desc}</div>;
+      })}
+    </div>
+  );
 };
 
 export default TransActionComponent;
