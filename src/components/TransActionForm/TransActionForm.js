754587d420abd77a3c9a7c52d3bbbdd6e018db8c
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-08-10 15:28:50 +0430

    styles added to TransactionForm

--- src/components/TransActionForm/TransActionForm.js
+++ src/components/TransActionForm/TransActionForm.js
@@ -1,32 +1,58 @@
 import { useState } from "react";
 
-const TransActionForm = ({addTransaction}) => {
+const TransActionForm = ({ addTransaction }) => {
   const [formValues, setFormValues] = useState({
     type: "expense",
     amount: 0,
     desc: "",
   });
 
-  const changeHandler=(e)=>{
-    setFormValues({...formValues, [e.target.name] :e.target.value});
-  }
+  const changeHandler = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
 
-  const submitHandler=(e)=>{
+  const submitHandler = (e) => {
     e.preventDefault();
-    addTransaction(formValues)
-  }
+    addTransaction(formValues);
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" name="desc" onChange={changeHandler} value={formValues.desc}/>
-      <input type="number" name="amount" onChange={changeHandler} value={formValues.amount} />
-      <div>
-        <input type="radio" value="expense" name="type" onChange={changeHandler} checked={formValues.type==="expense"}/>
-        <label>Expense</label>
-        <input type="radio" value="income" name="type" onChange={changeHandler} checked={formValues.type==="income"}/>
-        <label>Income</label>
+      <input
+        type="text"
+        name="desc"
+        onChange={changeHandler}
+        value={formValues.desc}
+      />
+      <input
+        type="number"
+        name="amount"
+        onChange={changeHandler}
+        value={formValues.amount}
+      />
+      <div className="radioBox">
+        <input
+          id="expense"
+          type="radio"
+          value="expense"
+          name="type"
+          onChange={changeHandler}
+          checked={formValues.type === "expense"}
+        />
+        <label htmlFor="expense">Expense</label>
+        <input
+          id="income"
+          type="radio"
+          value="income"
+          name="type"
+          onChange={changeHandler}
+          checked={formValues.type === "income"}
+        />
+        <label htmlFor="income">Income</label>
       </div>
-      <button type="submit" className="btn btn--primary">Add transaction</button>
+      <button type="submit" className="btn btn--primary">
+        Add transaction
+      </button>
     </form>
   );
 };
